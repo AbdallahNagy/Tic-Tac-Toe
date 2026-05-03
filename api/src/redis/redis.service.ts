f0f34a0ef7370bbcb14385e9722d3d7c4cb971f5
abdallah nagy <abdallahnagy773@gmail.com>
2026-05-03 14:56:16 +0300

    feat: integrate Redis with NestJS and add configuration support

---
+++ api/src/redis/redis.service.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+import { REDIS_CLIENT } from './redis.constants';
+
+@Injectable()
+export class RedisService {
+  constructor(@Inject(REDIS_CLIENT) private readonly client: Redis) {}
+
+  getClient(): Redis {
+    return this.client;
+  }
+
+  ping(): Promise<string> {
+    return this.client.ping();
+  }
+}

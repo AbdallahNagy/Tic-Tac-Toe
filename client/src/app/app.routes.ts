7067e1cbf17187763a506a224fadc2cdcac90505
abdallah nagy <abdallahnagy773@gmail.com>
2026-05-04 21:40:48 +0300

    chore: init lobby and board components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Lobby } from './features/lobby/lobby';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'lobby',
+        pathMatch: 'full'
+    },
+    {
+        path: 'lobby',
+        component: Lobby
+    },
+    {
+        path: 'board',
+        loadComponent: () => import('./features/board/board').then(m => m.Board)
+    }
+];

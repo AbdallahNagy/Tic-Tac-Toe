11dcc642bea1a43201bf84dc5ff823aa70ba3127
abdallah nagy <abdallahnagy773@gmail.com>
2026-05-06 23:28:41 +0300

    remove theme controller

--- client/src/app/features/lobby/lobby.ts
+++ client/src/app/features/lobby/lobby.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { ThemeControllerSunMoon } from '../../shared/components/theme-controller-sun-moon/theme-controller-sun-moon';
-import { StaticBoard } from "../../shared/components/static-board/static-board";
+import { StaticBoard } from '../../shared/components/static-board/static-board';
 
 @Component({
   selector: 'app-lobby',
-  imports: [
-    ThemeControllerSunMoon,
-    StaticBoard
-],
+  imports: [StaticBoard],
   templateUrl: './lobby.html',
   styleUrl: './lobby.css',
 })
--- client/src/app/shared/components/theme-controller-sun-moon/theme-controller-sun-moon.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-theme-controller-sun-moon',
-  imports: [],
-  templateUrl: './theme-controller-sun-moon.html',
-  styleUrl: './theme-controller-sun-moon.css',
-})
-export class ThemeControllerSunMoon {}
